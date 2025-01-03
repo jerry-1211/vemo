7de39dfb431ddbfd993afea7523d0d0742a658d7
jerry <kcjerim1234567@gmail.com>
2025-01-03 17:37:36 +0900

    진짜 되나 ?

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
     getHello(): string {
-        return 'WHY WHY WHY WHY !! ';
+        return 'Please Help me Pleasesese@@@ ';
     }
 }
