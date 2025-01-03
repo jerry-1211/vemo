be0a56555858a1ee0c766af666bf6fa7ca8ea58e
jerry <kcjerim1234567@gmail.com>
2025-01-03 14:50:05 +0900

    port 수정

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -36,7 +36,7 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
 
-    await app.listen(process.env.PORT ?? 5050);
+    await app.listen(process.env.PORT ?? 5050, '0.0.0.0');
 }
 
 bootstrap();
